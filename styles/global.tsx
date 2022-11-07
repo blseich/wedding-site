fe8a014f7137d34277ff5eefd6ab25e83f990721
Brandon Seich <blseich@gmail.com>
2022-11-07 17:42:19 -0500

    polish homepage

--- styles/global.tsx
+++ styles/global.tsx
@@ -6,9 +6,12 @@ const globalStyles = (
     styles={css`
       html,
       body {
-        background: ${base};
+        margin: 0;
         font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
         Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      }
+      * {
+        box-sizing: border-box;
       }
     `}
   />);
