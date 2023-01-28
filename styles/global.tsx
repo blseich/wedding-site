cf30e9c6c703643c499de660a200a43225caf5cc
Brandon Seich <blseich@gmail.com>
2023-01-28 17:11:15 -0500

    resolve white bar issue on mobile

--- styles/global.tsx
+++ styles/global.tsx
@@ -5,7 +5,8 @@ const globalStyles = (
   <Global 
     styles={css`
       html,
-      body {
+      body, #__next {
+        height: 100%;
         margin: 0;
         font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
         Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
