bff7399010ea412da8a8f53fcc0cd9195c57e142
Brandon Seich <blseich@gmail.com>
2023-01-29 11:01:53 -0500

    fix build errors

--- pages/our-story/MeetSection.tsx
+++ pages/our-story/MeetSection.tsx
@@ -14,6 +14,7 @@ const MeetSection = () => (
       src={how_we_became_us.src}
       height={how_we_became_us.height}
       width={how_we_became_us.width}
+      alt={'how we became us'}
     />
     <Image
       css={css`
@@ -27,12 +28,13 @@ const MeetSection = () => (
       src={short_north_pint_house.src}
       height={short_north_pint_house.height}
       width={short_north_pint_house.width}
+      alt={'an artsy picture of the short north pint house'}
     />
     <div>
       It was June 2019. Maddi had recently moved to Columbus and made a new group of girl friends, one of which included Bridesmade Neuhardt. One weekend, Brandon and Groomsman Larr called Bridesmade Neuhardt to invite her to the Jazz & Ribbs festival. She informed them she was downtown bar hopping with some girls. Ribs & Jazz were promptly forgotten.
     </div>
     <div>
-      Brandon and Groomsman Larr caught up with the group at Short North Pint House and joined the bar hop. At one point, Brandon ever-so-smoothly asked Maddi, "So, do you have a boyfriend?" Maddi laughed and replied with a "no". The two exchanged numbers with a promise to meet up in the future.
+      Brandon and Groomsman Larr caught up with the group at Short North Pint House and joined the bar hop. At one point, Brandon ever-so-smoothly asked Maddi, &quot;So, do you have a boyfriend?&quot; Maddi laughed and replied with a &quot;no&quot;. The two exchanged numbers with a promise to meet up in the future.
     </div>
     <div>
       To this day, Brandon still has not been to the Ribs & Jazz festival...
